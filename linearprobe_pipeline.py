8195c77ffc954be2ab1dc2d331e7dc5b4078a363
Ahmad Mustapha <ahmad.m.mustapha@hotmail.com>
2025-12-08 17:13:49 +0200

    Test weights

--- linearprobe_pipeline.py
+++ linearprobe_pipeline.py
@@ -117,6 +117,7 @@ def probe(encoder_name, dataset_name, variance_weighting_strategy= None, batch_s
                 features = get_features(encoder, inputs, encoder_target_dim, device="cuda")
             if variance_weighting_strategy:
                 variance_tracker.update(features)
+                features = features.detach().requires_grad_(True)
             if variance_weighting_strategy == VarianceWeightingStrategy.FEATURE_MULTIPLY:
                 features = variance_tracker.apply_weights(features)
             outputs = classifier(features)
