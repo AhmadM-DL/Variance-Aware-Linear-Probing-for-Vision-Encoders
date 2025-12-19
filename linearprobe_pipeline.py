7934f8187a1b54b1aa7612da857549b9be95c881
Ahmad Mustapha <ahmad.m.mustapha@hotmail.com>
2025-12-19 17:20:55 +0200

    Hope :-)

--- linearprobe_pipeline.py
+++ linearprobe_pipeline.py
@@ -221,9 +221,11 @@ def probe(encoder_name, dataset_name, boost_with_variance= False, batch_size= 64
                 elif boosting_method == BoostingMethod.B_GRADIENTS:
                     threshold = torch.quantile(var_weights, boosting_percentile_threshold)
                     boost_mask = var_weights >= threshold
-                    dim_mask = ~boost_mask
                     var_weights[boost_mask] = var_weights[boost_mask] * boosting_scale
-                    var_weights[dim_mask] = 0
+                    var_weights[~boost_mask] = 0
+
+                    var_weights[~boost_mask] = torch.where(var_weights[~boost_mask] > 0, -var_weights[~boost_mask], var_weights[~boost_mask])
+
                     grad_booster.set(var_weights)
                     outputs = classifier(features)
                 elif boosting_method == BoostingMethod.WEIGHTS:
